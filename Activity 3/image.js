5270c6c12bb4e2884964e4e2365acd928884e8f1
Hydro021 <163384241+Hydro021@users.noreply.github.com>
2024-03-23 11:34:26 +0800

    Update image.js

--- Activity 3/image.js
+++ Activity 3/image.js
@@ -1,8 +1,9 @@
-const rightdiv =(image)=>{
-    let mydiv = document.createElement("mydiv");
-    mydiv.className="RightClass";
-    mydiv.innerHTML=`<img src=" /Activity 3/${image}">`
+const rightdiv = (image) => {
+    let mydiv = document.createElement("div"); // Change "mydiv" to "div"
+    mydiv.className = "RightClass";
+    mydiv.innerHTML = `<img src="./${image}">`; // Adjust image path
 
     return mydiv;
 }
-export{rightdiv}
+
+export { rightdiv };
