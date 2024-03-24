097492a267c60fd436983024085c91e6e8589f1d
Hydro021 <163384241+Hydro021@users.noreply.github.com>
2024-03-24 21:39:39 +0800

    Delete Activity 3 directory

--- Activity 3/image.js
+++
@@ -1,9 +0,0 @@
-const rightdiv = (image) => {
-    let mydiv = document.createElement("div"); // Change "mydiv" to "div"
-    mydiv.className = "RightClass";
-    mydiv.innerHTML = `<img src="./${image}">`; // Adjust image path
-
-    return mydiv;
-}
-
-export { rightdiv };
